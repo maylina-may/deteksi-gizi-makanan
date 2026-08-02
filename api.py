30c01feeecdb9da58b0d8732e5ea1bd59bad9c7a
Maylina Nur'aini <maylinanuraini@gmail.com>
2026-08-02 12:12:41 +0700

    Update api.py

--- api.py
+++ api.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 CORS(app)
 
 # ========== Konfigurasi Groq ==========
-KEY = os.environ.get("GROQ_API_KEY") or "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"
+KEY = os.environ.get("GROQ_API_KEY") or "gsk_xxxxx"
 MODEL = "llama-3.3-70b-versatile"  # Model yang tersedia di Groq
 
 # ========== Load YOLO Model ==========
